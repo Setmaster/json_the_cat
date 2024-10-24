6997d0684279c5da4d310bee0c14143acc411c26
Vitor <vi7org@hotmail.com>
2024-10-24 13:24:37 -0400

    Refactor and add tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,21 @@
 ﻿const needle = require('needle');
 const BASEURL = "https://api.thecatapi.com/v1/breeds/";
-const BREED_NAME = process.argv[2];
 
-const fetchBreed = async function() {
-  try {
-    const response = await needle('get', `${BASEURL}search?q=${BREED_NAME}`);
-
-    if (response.statusCode !== 200) {
-      console.error(`Status code error: ${response.statusCode}`);
-      process.exit(-1);
-    }
-
-    if (response.body.length === 0) {
-      console.error(`Error: Breed not found`);
-      process.exit(-1);
-    }
-        
-    console.log(response.body[0]["description"]);
-        
-  } catch (error) {
-    console.error(`${error}`);
-    process.exit(-1);
-  }
+const fetchBreedDescription = function(name, callback) {
+    needle('get', `${BASEURL}search?q=${name}`, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else if (response.statusCode !== 200) {
+        callback(`Status code error: ${response.statusCode}`, null);
+      } else {
+        const body = response.body;
+        if (body && body.length > 0) {
+          callback(null, body[0]["description"]);
+        } else {
+          callback("No breed found", null);
+        }
+      }
+    });
 };
 
-fetchBreed();
+module.exports = {fetchBreedDescription}
